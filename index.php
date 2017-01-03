2201c961662f0281950e27fdcc2639859e7e5ff6
Israel Aviñante <avinante.israel@gmail.com>
2017-01-03 09:33:40 +0800

    Content removed

--- index.php
+++ index.php
@@ -7,25 +7,3 @@
 		<li><a href="#contact">Contact Us</a></li>
 	</ul>
 </nav>
-
-<div class="content-container">
-  <div id="home" class="content">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc a urna elementum rhoncus. Praesent tincidunt viverra eros, ut tempor nibh rutrum vitae. Vestibulum augue enim, pellentesque in bibendum ac, scelerisque id nisl. Nam ante velit, egestas non gravida non, facilisis facilisis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae dui fermentum, dictum quam ut, feugiat nulla. Nullam maximus ex eget purus commodo maximus. Sed sed leo at erat convallis luctus. Nullam ac malesuada odio. Nullam at mollis eros, ut tempus ipsum. In eu condimentum mauris. Maecenas eu bibendum ante.</p>
-  </div>
-
-  <div id="about" class="content">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc a urna elementum rhoncus. Praesent tincidunt viverra eros, ut tempor nibh rutrum vitae. Vestibulum augue enim, pellentesque in bibendum ac, scelerisque id nisl. Nam ante velit, egestas non gravida non, facilisis facilisis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae dui fermentum, dictum quam ut, feugiat nulla. Nullam maximus ex eget purus commodo maximus. Sed sed leo at erat convallis luctus. Nullam ac malesuada odio. Nullam at mollis eros, ut tempus ipsum. In eu condimentum mauris. Maecenas eu bibendum ante.</p>
-  </div>
-
-  <div id="product" class="content">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc a urna elementum rhoncus. Praesent tincidunt viverra eros, ut tempor nibh rutrum vitae. Vestibulum augue enim, pellentesque in bibendum ac, scelerisque id nisl. Nam ante velit, egestas non gravida non, facilisis facilisis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae dui fermentum, dictum quam ut, feugiat nulla. Nullam maximus ex eget purus commodo maximus. Sed sed leo at erat convallis luctus. Nullam ac malesuada odio. Nullam at mollis eros, ut tempus ipsum. In eu condimentum mauris. Maecenas eu bibendum ante.</p>
-  </div>
-
-  <div id="services" class="content">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc a urna elementum rhoncus. Praesent tincidunt viverra eros, ut tempor nibh rutrum vitae. Vestibulum augue enim, pellentesque in bibendum ac, scelerisque id nisl. Nam ante velit, egestas non gravida non, facilisis facilisis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae dui fermentum, dictum quam ut, feugiat nulla. Nullam maximus ex eget purus commodo maximus. Sed sed leo at erat convallis luctus. Nullam ac malesuada odio. Nullam at mollis eros, ut tempus ipsum. In eu condimentum mauris. Maecenas eu bibendum ante.</p>
-  </div>
-
-  <div id="contact" class="content">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nunc a urna elementum rhoncus. Praesent tincidunt viverra eros, ut tempor nibh rutrum vitae. Vestibulum augue enim, pellentesque in bibendum ac, scelerisque id nisl. Nam ante velit, egestas non gravida non, facilisis facilisis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae dui fermentum, dictum quam ut, feugiat nulla. Nullam maximus ex eget purus commodo maximus. Sed sed leo at erat convallis luctus. Nullam ac malesuada odio. Nullam at mollis eros, ut tempus ipsum. In eu condimentum mauris. Maecenas eu bibendum ante.</p>
-  </div>
-</div>
