46a4df5eb1a5bd6b755fdbebdba6f2346fa57009
Israel Avinante <avinante.israel@gmail.com>
2018-04-10 10:16:07 +0800

    Convert this to plugin

--- smooth-scroll/smooth-scroll.js
+++ smooth-scroll/smooth-scroll.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){
+/* jQuery(document).ready(function($){
 	// Smooth scroll
 	$('.ia-smooth-scroll').find('a').on('click', function(event){
 		// Make sure this.hash has a value before overriding default behavior
@@ -20,10 +20,53 @@ jQuery(document).ready(function($){
 		var anchorHash = $(this).prop('hash');
 
 		$(document).on('scroll', function(){
-			if($(this).scrollTop()>=$(anchorHash).position().top - 50){
-				$('.ia-smooth-scroll').find('a').parent().removeClass('active');
-				$('.ia-smooth-scroll').find('a[href="' + anchorHash + '"]').parent().addClass('active');
+			if($(this).scrollTop() >= $(anchorHash).position().top - 50){
+				$('#main-menu .ia-smooth-scroll').find('a').parent().removeClass('active');
+				$('#main-menu .ia-smooth-scroll').find('a[href="' + anchorHash + '"]').parent().addClass('active');
 			}
 		});
 	});
-});
+}); */
+
+(function($){
+	$.fn.smoothyScrolly = function(){
+		var selector = this.prop("class");
+
+		// Smooth scroll
+		this.find('a').on('click', function(event){
+
+			// Make sure this.hash has a value before overriding default behavior
+			if(this.hash !== ""){
+
+				// Prevent default anchor click behavior
+				event.preventDefault();
+
+				// Store hash
+				var hash = this.hash;
+
+				// Smooth scroll animation
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top - 50
+				}, 800);
+			}
+		});
+
+		// Add "active" class
+		this.find('a').each(function(){
+			var anchorHash 	= $(this).prop('hash');
+
+			$(document).on('scroll', function(){
+				if($(this).scrollTop() >= $(anchorHash).position().top - 50){
+
+					// Remove all "active" class
+					$('#main-menu').find('a').parent().removeClass('active');
+
+					// Add "active" class to the current menu
+					$('#main-menu').find('a[href="' + anchorHash + '"]').parent().addClass('active');
+				}
+			});
+		});
+
+		return this;
+	};
+}(jQuery));
